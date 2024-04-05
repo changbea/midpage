be206e277b1d40a23be16d7998d8deea5e8dd507
changbea <68812384+changbea@users.noreply.github.com>
2024-04-06 01:00:38 +0900

    Adding

--- src/Navigate.jsx
+++ src/Navigate.jsx
@@ -5,22 +5,22 @@ import './Navigate.css'
 export default function Navigate() {
     return (
         <div className='nav'>
-            <Link to="/newpage/">
+            <Link to="/midpage/">
                 <img src={logo} alt='react' width='5%' height='5%' />
             </Link>
             <br />
-            <Link to="/newpage/drink">Drink&emsp;</Link>
-            <Link to="/newpage/subway">Subway&emsp;</Link>
-            <Link to="/newpage/cards">Cards&emsp;</Link>
-            <Link to="/newpage/recommendations">Recommendations&emsp;</Link>
-            <Link to="/newpage/resume">Resume&emsp;</Link>
-            <Link to="/newpage/leagues">Leagues&emsp;</Link>
-            <Link to="/newpage/gamersp">Gamersp&emsp;</Link>
-            <Link to="/newpage/gamesrspextended">Gamesrspextended&emsp;</Link>
-            <Link to="/newpage/gamefinding">Gamefinding&emsp;</Link>
-            <Link to="/newpage/gameflipping">Gameflipping&emsp;</Link>
-            <Link to="/newpage/gamehint">Gamehint&emsp;</Link>
-            <Link to="/newpage/planning">Planning&emsp;</Link>
+            <Link to="/midpage/drink">Drink&emsp;</Link>
+            <Link to="/midpage/subway">Subway&emsp;</Link>
+            <Link to="/midpage/cards">Cards&emsp;</Link>
+            <Link to="/midpage/recommendations">Recommendations&emsp;</Link>
+            <Link to="/midpage/resume">Resume&emsp;</Link>
+            <Link to="/midpage/leagues">Leagues&emsp;</Link>
+            <Link to="/midpage/gamersp">Gamersp&emsp;</Link>
+            <Link to="/midpage/gamesrspextended">Gamesrspextended&emsp;</Link>
+            <Link to="/midpage/gamefinding">Gamefinding&emsp;</Link>
+            <Link to="/midpage/gameflipping">Gameflipping&emsp;</Link>
+            <Link to="/midpage/gamehint">Gamehint&emsp;</Link>
+            <Link to="/midpage/planning">Planning&emsp;</Link>
         </div>
     );      
 }
